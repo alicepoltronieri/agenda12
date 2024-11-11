ff5d5b8c80fb99e905cd51f427d93a08aee65825
Alice Poltronieri <alice.snieri@hotmail.com>
2024-11-11 19:57:42 -0300

    Adicionar arquivos de projeto.

---
+++ agenda12/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace agenda12
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ agenda12/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace agenda12
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
